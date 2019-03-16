456897e54ff478d66b0e4764c7ab58935fa75f1e
zhuyun <zjut_zy@163.com>
2019-03-16 13:29:54 +0800

    v0.5

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, redirect, url_for, render_template, send_from_directory, abort
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory, abort, jsonify
 import fasttext
+import traceback
 import os
 # from werkzeug.utils import secure_filename
 import jieba
 # import csv
-
 
 
 
@@ -17,23 +17,35 @@ def create_app():
     def hello_world():
         return '欢迎来到猪事顺心小组的作品。\n该项目正在开发中，敬请期待！'
 
-    @app.route("/upload", methods=["GET"])
-    def upload_one():
-        # 获取get的参数
-        pn = request.args.get("productname")
-        if not fasttext_model:
-            fasttext_model = fasttext.load_model("./algorithm/a.bin", label_prefix='__label__')
+    @app.route("/upload")
+    def upload_one(fasttext_model = fasttext_model):
         try:
-            result = fasttext_model.predict(pn)[0][0]  # 是否要返回前三个？
-            return {
-                "result": result,
-                "code": 200
-            }
-        except err:
-            return {
-                "code": 500,
+            # 获取get的参数
+            pn = request.args.get("productname")
+            if not fasttext_model:
+                fasttext_model = fasttext.load_model("./algorithm/a.bin", label_prefix='__label__')
+            resultlist = fasttext_model.predict(pn)[0]  # 是否要返回前三个？
+            if resultlist:
+                result = resultlist[0]
+                return jsonify({
+                    "result": result,
+                    "code": 0
+                })
+            else:
+                return jsonify({
+                    "result": "无法预测",
+                    "code": 1
+                })
+        except Exception as e:
+            traceback.print_exc()
+            # 或者得到堆栈字符串信息
+            info = traceback.format_exc()
+            print(info, str(e))
+            return jsonify({
+                "code": 2,
                 "message": "错误发生在预测时"
-            }
+            })
+
     return app
 
 
--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,6 @@ from flask import Flask
 application = create_app()
 
 if __name__ == '__main__':
+    # 返回的json可以显示中文
+    application.config['JSON_AS_ASCII'] = False
     application.run()
