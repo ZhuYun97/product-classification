2143108ec991b27b215854bda8563cd877f39e9a
zhuyun <zjut_zy@163.com>
2019-03-16 16:47:50 +0800

    v0.5

--- algorithm/train_model.py
+++ algorithm/train_model.py
@@ -7,4 +7,6 @@ import time
 basedir = './'
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-classifier = fasttext.supervised(basedir + "highrate.train", basedir + "a", label_prefix="__label__", dim=180, ws=5, neg=5, epoch=150, min_count=2, lr=0.1, lr_update_rate=1000, bucket=100000)
+classifier = fasttext.supervised(basedir + "highrate.train", basedir + "b", label_prefix="__label__", dim=200, ws=5, neg=5, epoch=100, min_count=2, lr=0.1, lr_update_rate=1000, bucket=100000)
+classifier = fasttext.supervised(basedir + "highrate.train", basedir + "c", label_prefix="__label__", dim=150, ws=5, neg=5, epoch=80, min_count=2, lr=0.1, lr_update_rate=1000, bucket=100000)
+classifier = fasttext.supervised(basedir + "highrate.train", basedir + "d", label_prefix="__label__", dim=220, ws=5, neg=5, epoch=50, min_count=2, lr=0.1, lr_update_rate=1000, bucket=100000)
