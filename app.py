e5d3a4d73e28aa756abd668afe9138965bea2318
zhuyun <zjut_zy@163.com>
2019-03-16 10:42:06 +0800

    v0.2

--- app.py
+++ app.py
@@ -5,37 +5,35 @@ import os
 import jieba
 # import csv
 
-fasttext_model = None
-file_name=""
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route("/upload", methods=["GET"])
-def upload_one():
-    # 获取get的参数
-    pn = request.args.get("productname")
-    if not fasttext_model:
-        fasttext_model = fasttext.load_model("./algorithm/a.bin", label_prefix='__label__')
-    try:
-        result = fasttext_model.predict(pn)[0][0]  # 是否要返回前三个？
-        return {
-            "result": result,
-            "code": 200
-        }
-    except err:
-        return {
-            "code": 500,
-            "message": "错误发生在预测时"
-        }
 
 
 def create_app():
     app = Flask(__name__)
     fasttext_model = fasttext.load_model("./algorithm/a.bin", label_prefix='__label__')
+    file_name = ""
+
+    @app.route('/')
+    def hello_world():
+        return '欢迎来到猪事顺心小组的作品。\n该项目正在开发中，敬请期待！'
+
+    @app.route("/upload", methods=["GET"])
+    def upload_one():
+        # 获取get的参数
+        pn = request.args.get("productname")
+        if not fasttext_model:
+            fasttext_model = fasttext.load_model("./algorithm/a.bin", label_prefix='__label__')
+        try:
+            result = fasttext_model.predict(pn)[0][0]  # 是否要返回前三个？
+            return {
+                "result": result,
+                "code": 200
+            }
+        except err:
+            return {
+                "code": 500,
+                "message": "错误发生在预测时"
+            }
     app.run()
 
 
--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,5 @@ from flask import Flask
 
 application = create_app()
 
-application = create_app()
-
 if __name__ == '__main__':
     application.run()
