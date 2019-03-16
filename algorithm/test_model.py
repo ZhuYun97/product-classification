8c896b50e76a2fb8fded64d4c3fc59f082976028
zhuyun <zjut_zy@163.com>
2019-03-16 13:44:56 +0800

    v0.5

--- algorithm/test_model.py
+++ algorithm/test_model.py
@@ -2,8 +2,12 @@ import fasttext
 classifier1 = fasttext.load_model("./a.bin", label_prefix='__label__')
 #classifier2 = fasttext.load_model("./b.bin", label_prefix='__label__')
 
-result = classifier1.test("./highrate.test")
-print("Precision on testing data:", result.precision)
-result = classifier1.test("./highrate.train")
-print("Precision on training data:", result.precision)
+# result = classifier1.test("./highrate.test")
+# print("Precision on testing data:", result.precision)
+# result = classifier1.test("./highrate.train")
+# print("Precision on training data:", result.precision)
+result = classifier1.predict("QQ充值红钻")
+print(result[0][0])
+result = classifier1.predict("兰亭集序")
+print(result[0][0])
 
