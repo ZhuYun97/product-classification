bc35e183d90de526edf7ebd09a69a9e892220d73
zhuyun <zjut_zy@163.com>
2019-03-17 10:11:41 +0800

    v0.5

--- algorithm/train_model.py
+++ algorithm/train_model.py
@@ -10,3 +10,6 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 classifier = fasttext.supervised(basedir + "highrate.train", basedir + "b", label_prefix="__label__", dim=200, ws=5, neg=5, epoch=100, min_count=2, lr=0.1, lr_update_rate=1000, bucket=100000)
 classifier = fasttext.supervised(basedir + "highrate.train", basedir + "c", label_prefix="__label__", dim=150, ws=5, neg=5, epoch=80, min_count=2, lr=0.1, lr_update_rate=1000, bucket=100000)
 classifier = fasttext.supervised(basedir + "highrate.train", basedir + "d", label_prefix="__label__", dim=220, ws=5, neg=5, epoch=50, min_count=2, lr=0.1, lr_update_rate=1000, bucket=100000)
+classifier = fasttext.supervised(basedir + "highrate.train", basedir + "e", label_prefix="__label__", dim=200, ws=5, neg=5, epoch=20, min_count=2, lr=0.1, lr_update_rate=1000, bucket=200000)
+classifier = fasttext.supervised(basedir + "highrate.train", basedir + "f", label_prefix="__label__", dim=100, ws=5, neg=5, epoch=20, min_count=2, lr=0.1, lr_update_rate=1000, bucket=100000)
+classifier = fasttext.supervised(basedir + "highrate.train", basedir + "b", label_prefix="__label__", dim=50, ws=5, neg=5, epoch=50, min_count=2, lr=0.1, lr_update_rate=1000, bucket=100000)
