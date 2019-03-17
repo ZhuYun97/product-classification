51c5711d79f7c0d8d1b556f7b659e902beeacddb
zhuyun <zjut_zy@163.com>
2019-03-17 16:36:15 +0800

    v0.5

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ def create_app():
             # resultlist = fasttext_model.predict([pn], 5)
             prolist = fasttext_model.predict_proba([pn], 3)
             return jsonify({
+                "pn": pn,
                 "code": 0,
                 "problist": prolist
             })
