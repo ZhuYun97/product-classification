5178dc505b84c10246bf7ef6e0b87b90c5385e9d
zhuyun <zjut_zy@163.com>
2019-03-22 13:39:50 +0800

    v0.8

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
 # wsgi.py
 from app import create_app
+from flask_cors import *
+
 
 application = create_app()
 
 if __name__ == '__main__':
+    CORS(application, supports_credentials=True)
     application.run()
